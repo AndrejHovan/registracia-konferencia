3cb67253bcff1708c16791d5e7adcf6b6473ab34
student <student@158.197.35.79>
2018-10-17 09:31:50 +0200

    vsetko updatenute

--- src/main/java/sk/upjs/registracia_konferencia/Companion.java
+++ src/main/java/sk/upjs/registracia_konferencia/Companion.java
@@ -3,8 +3,57 @@ package sk.upjs.registracia_konferencia;
 import java.time.LocalDateTime;
 
 public class Companion {
+	
+	public static final double[] COMPANION_FEES = new double[]{60.0, 110.0,165.0,210.0};
+	public static final double BANKET_FEE = 20.0;
+	public static final LocalDateTime BANKET_DATETIME = LocalDateTime.of(2019, 9, 23, 19, 0); 
+	public static final double TSHIRT_FEE = 5.0;
+	
 	private CompanionCategory category;
 	private LocalDateTime start;
 	private LocalDateTime end;
 	private Tshirt tshirt;
+	
+	public double getPrice() {
+		double price = 0.0;
+		int nights = start.toLocalDate().until(end.toLocalDate()).getDays();
+		if (nights > 0) {
+			price = COMPANION_FEES[nights-1];
+		}
+		if(start.isBefore(BANKET_DATETIME) && end.isAfter(BANKET_DATETIME)) {
+			price += BANKET_FEE;
+		}
+		if (tshirt != null) {
+			price += TSHIRT_FEE;
+		}
+		
+		return price;
+	}	
+	
+	public CompanionCategory getCategory() {
+		return category;
+	}
+	public void setCategory(CompanionCategory category) {
+		this.category = category;
+	}
+	public LocalDateTime getStart() {
+		return start;
+	}
+	public void setStart(LocalDateTime start) {
+		this.start = start;
+	}
+	public LocalDateTime getEnd() {
+		return end;
+	}
+	public void setEnd(LocalDateTime end) {
+		this.end = end;
+	}
+	public Tshirt getTshirt() {
+		return tshirt;
+	}
+	public void setTshirt(Tshirt tshirt) {
+		this.tshirt = tshirt;
+	}
+	
+	
 }
--- src/main/java/sk/upjs/registracia_konferencia/Participant.java
+++ src/main/java/sk/upjs/registracia_konferencia/Participant.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class Participant {
 	
+	public static final double SINGLE_ROOM_FEE = 75.0;
+
+	private Long id;
 	private String name;
 	private String surname;
 	private String email;
@@ -22,5 +25,159 @@ public class Participant {
 	private Workshop workshop;
 	private boolean cash;
 	
+	public double finalPrice() {
+		
+		double price = (isStudent()) ? (early ? workshop.getPriceStudent() : workshop.getPriceStudentLate()) : 
+			(early ? workshop.getPriceFull(): workshop.getPriceFullLate());
+		if(singleRoom)
+			price += SINGLE_ROOM_FEE;
+		if(companions != null) {
+			for (Companion c : companions) {
+				price += c.getPrice();
+			}
+		}
+		return price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getOrganization() {
+		return organization;
+	}
+
+	public void setOrganization(String organization) {
+		this.organization = organization;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Long getIco() {
+		return ico;
+	}
+
+	public void setIco(Long ico) {
+		this.ico = ico;
+	}
+
+	public String getDic() {
+		return dic;
+	}
+
+	public void setDic(String dic) {
+		this.dic = dic;
+	}
+
+	public boolean isEarly() {
+		return early;
+	}
+
+	public void setEarly(boolean early) {
+		this.early = early;
+	}
+
+	public Tshirt getTshirt() {
+		return tshirt;
+	}
+
+	public void setTshirt(Tshirt tshirt) {
+		this.tshirt = tshirt;
+	}
+
+	public boolean isStudent() {
+		return isStudent;
+	}
+
+	public void setStudent(boolean isStudent) {
+		this.isStudent = isStudent;
+	}
+
+	public boolean isSingleRoom() {
+		return singleRoom;
+	}
+
+	public void setSingleRoom(boolean singleRoom) {
+		this.singleRoom = singleRoom;
+	}
+
+	public LocalDateTime getStart() {
+		return start;
+	}
+
+	public void setStart(LocalDateTime start) {
+		this.start = start;
+	}
+
+	public LocalDateTime getEnd() {
+		return end;
+	}
+
+	public void setEnd(LocalDateTime end) {
+		this.end = end;
+	}
+
+	public List<Companion> getCompanions() {
+		return companions;
+	}
+
+	public void setCompanions(List<Companion> companions) {
+		this.companions = companions;
+	}
+
+	public Workshop getWorkshop() {
+		return workshop;
+	}
+
+	public void setWorkshop(Workshop workshop) {
+		this.workshop = workshop;
+	}
+
+	public boolean isCash() {
+		return cash;
+	}
+
+	public void setCash(boolean cash) {
+		this.cash = cash;
+	}
 	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Participant [id=" + id + ", name=" + name + ", surname=" + surname + ", email=" + email + "]";
+	}
+
 }
